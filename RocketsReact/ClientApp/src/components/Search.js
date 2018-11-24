e7aaeb22c12d351e848343c5d7611fbdb4b37226
Michael Marks <mmarks315@charter.net>
2018-11-24 16:22:46 -0600

    messing with React

---
+++ RocketsReact/ClientApp/src/components/Search.js
@@ -0,0 +1,42 @@
+﻿import React from 'react';
+import PropTypes from "prop-types";
+import ReactDOM from 'react-dom';
+import SearchResult from './SearchResult';
+
+export class Search extends React.Component {
+    displayName = Search.name
+
+    state = {
+        searchInput: '',
+        performSearch: false
+    };
+
+    handleSearchButtonClicked = () => {
+        //alert('You clicked me!');
+
+        this.setState({            
+            performSearch: true
+        });
+    }
+
+    updateInputValue(evt) {
+        this.setState({
+            searchInput: evt.target.value,
+            performSearch: false
+        });
+    }    
+
+    render() {
+        return (
+            <div>
+                <h1>Search</h1>
+                <div>
+                    <input type='text' value={this.state.searchInput} onChange={evt => this.updateInputValue(evt)}></input>&nbsp;&nbsp;
+                    <button onClick={this.handleSearchButtonClicked}>Search</button>
+                    <br />
+                    <SearchResult performSearch={this.state.performSearch} searchInput={this.state.searchInput}/>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ RocketsReact/ClientApp/src/components/SearchResult.js
@@ -0,0 +1,25 @@
+﻿import React from 'react';
+
+class SearchResult extends React.Component {
+
+    render() {
+        if (this.props.performSearch === true) {
+            return (
+                <div>
+                    <p>Changed</p>
+                </div>
+            );
+        } else {
+
+            return (
+                <div>
+                    <p>Some text</p>
+                </div>
+            );
+        }
+
+        
+    }
+}
+
+export default SearchResult;
