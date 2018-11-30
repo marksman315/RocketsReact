2cb705666fb0d8ae1cf9a62d47bee3dd77eb9099
Michael Marks <mmarks315@charter.net>
2018-11-30 14:11:59 -0600

    Returning results!

--- RocketsReact/ClientApp/src/components/SearchResultList.js
+++ RocketsReact/ClientApp/src/components/SearchResultList.js
@@ -7,14 +7,25 @@ class SearchResultList extends React.Component {
             return (
                 <div>
                     <h2>Results</h2>
-                    <p>Changed</p>
+                    {this.props.results.result.map((result) => (
+                        <div>
+                            <span>id: {result.id}</span><br />
+                            <span>location: {result.location}</span><br />
+                            <span>model: {result.model}</span><br />
+                            <span>stages: {result.stages}</span><br />
+                            <span>type: {result.type}</span><br />
+                            <br />
+                        </div>
+                    ))}
+                    <br />
+                    <p>Hooray!!!</p>
                 </div>
             );
         } else {
 
             return (
                 <div>
-                    <p>Some text</p>
+                    <p>Do a search, go ahead do it!</p>
                 </div>
             );
         }
